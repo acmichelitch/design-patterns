f2b992cc2ccdc8ddf3d203b507dc8a183737e426
Anna <acmichelitch@gmail.com>
2021-09-20 13:21:52 -0400

    added required classes

---
+++ iterator/FlightIterator.java
@@ -0,0 +1,22 @@
+package iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class FlightIterator implements Iterator {
+    
+    private Flight[] flights;
+    private int position;
+
+    public FlightIterator(Flight[] flights) {
+        //TODO: implement constructor
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public Flight next() {
+        return null;
+    }
+}
