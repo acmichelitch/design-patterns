68856b5a10f278179e240d44a398369dddf281a5
Anna <acmichelitch@gmail.com>
2021-11-24 20:30:11 -0500

    added javadoc

--- adapter/MedicalRecordAdapter.java
+++ adapter/MedicalRecordAdapter.java
@@ -1,3 +1,9 @@
+/**
+ * supporter class that gives a HealthRecord object the same behaviors as a MedicalRecord object
+ * @author anna michelitch
+ * completed 11/24/2021
+ */
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -10,24 +16,44 @@ public class MedicalRecordAdapter implements MedicalRecord {
 
     private HealthRecord record;
 
+    /**
+     * constructor for MedicalRecordAdapter
+     * @param record the HealthRecord object to be adapted into a MedicalRecord
+     */
     public MedicalRecordAdapter(HealthRecord record) {
         this.record = record;
     }
 
+    /**
+     * parses the HealthRecord's name to return first name
+     * @return first name
+     */
     public String getFirstName() {
         String[] fullName = record.getName().split(" ");
         return fullName[0];
     }
 
+    /**
+     * parses the HealthRecord's name to return last name
+     * @return last name
+     */
     public String getLastName() {
         String[] fullName = record.getName().split(" ");
         return fullName[1];
     }
 
+    /**
+     * accessor for birthday
+     * @return Date object representing birthday
+     */
     public Date getBirthday() {
         return record.getBirthdate();
     }
 
+    /**
+     * reads the health record's gender and returns the corresponding Gender emulation
+     * @return enum of the person's gender
+     */
     public Gender getGender() {
         if (record.getGender().equalsIgnoreCase("male"))
             return Gender.MALE;
@@ -36,10 +62,20 @@ public class MedicalRecordAdapter implements MedicalRecord {
         return Gender.OTHER;
     }
 
+    /**
+     * adds a new visit to the person's medical history
+     * @param date the date of the visit
+     * @param well boolean representing whether the visit was well or not (sick)
+     * @param description a comment describing the visit
+     */
     public void addVisit(Date date, boolean well, String description) {
         record.addHistory(date, well, description);
     }
 
+    /**
+     * parses through the HealthRecord's arraylist of strings to extract the correct information from each visit and creates a corresponding arraylist of Visits
+     * @return an arraylist of Visit objects representing the visit history
+     */
     public ArrayList<Visit> getVisitHistory() {
         
         ArrayList<String> visitHistory = record.getHistory();
@@ -78,6 +114,10 @@ public class MedicalRecordAdapter implements MedicalRecord {
         return visits;
     }
 
+    /**
+     * converts the HealthRecord's information into a string matching the MedicalRecord format
+     * @return string representing all of the information
+     */
     public String toString() {
         String result = "***** " + record.getName() + " *****\n";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy"); //ASK PORTIA WHY dd/MM are switched in output text
